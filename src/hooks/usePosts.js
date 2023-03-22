61848501738b16ba1c763d02e2027ca2a37f83ae
Hathaway-Shan <hathaway.shan@gmail.com>
2023-03-21 19:46:49 -0700

    imports started working again added usePost useEffect hook to hooks

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { fetchPosts } from '../services/reddit';
+
+export function usePosts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetchPosts();
+        setPosts(response);
+        setLoading(false);
+      } catch (e) {
+        setError(e.message);
+        setLoading(false);
+      }
+    };
+    loadData();
+  }, []);
+
+  return { posts, error, loading };
+}
