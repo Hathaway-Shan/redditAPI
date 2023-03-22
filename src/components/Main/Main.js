d76e0b05e6d28e99f8ad49ffb5f4c9ff96c8b68a
Hathaway-Shan <hathaway.shan@gmail.com>
2023-03-22 16:21:08 -0700

    changed max-width for better card display

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,7 +6,7 @@ export default function Main() {
   const { posts, error, loading } = usePosts();
   return (
     <div className="main">
-      <span>{error && <>{error}</>}</span>
+      <span>{error && <p className="error-message">{error}</p>}</span>
       <span>{loading && <h2 className="loading">Loading...</h2>}</span>
       <div className="post-box">
         {posts.map(({ data }) => (
