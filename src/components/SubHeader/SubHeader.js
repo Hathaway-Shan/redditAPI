9e25b7d8272b80ef084faf6f814069679e23a5f7
Hathaway-Shan <hathaway.shan@gmail.com>
2023-03-21 17:08:54 -0700

    created dual color sub header element and basic project layout

---
+++ src/components/SubHeader/SubHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SubHeader.css';
+
+export default function SubHeader() {
+  return (
+    <div className="SubHeader-container">
+      <h1>
+        r\<span className="SubHeader-greentext">Business</span>
+      </h1>
+    </div>
+  );
+}
---
+++ src/services/reddit.js
@@ -0,0 +1,7 @@
+export async function fetchPosts() {
+  const response = await fetch(`https://www.reddit.com/r/business/new.json`);
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
